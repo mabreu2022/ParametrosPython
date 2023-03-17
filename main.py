df9f17cc879cd09d37d6a27f69f8bf0f1f6faddd
Mauricio Marcelo Paiva de Abreu <mauricio.abreu@conectsolutionsti.com.br>
2023-03-17 09:14:58 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+cotacao_dolar = 0
+cotacao_euro = 0
+cotacao_bitCoinns = 0
+
+def funcCotacao (aTipoCotacao):
+    if aTipoCotacao == 1: # Dólar
+        print(f'O Tipo de Moeda é: Dólar')
+        #cotacao_dolar = # Código que faz a cotação do Dólar
+
+    if aTipoCotacao == 2: # Euro
+        print(f'O Tipo de Moeda é: Euro')
+        #cotacao_euro = # Código que faz a cotação do Dólar
+
+    if aTipoCotacao == 3: # BitCoins
+        print(f'O Tipo de Moeda é: BitCoins')
+        #cotacao_bitCoind
+
+Cot = int(input("Informe : 1-Paara Dólar; 2-Para Euro; 3-Para BitCoins"))
+
+funcCotacao(Cot)
+
+
